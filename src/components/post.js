2242c28e888db8279c3ada01d089fb9a8e82e7e4
Anthony Cavallaro <anthonyjcavallaro@outlook.com>
2020-11-08 00:32:56 -0500

    Convert media to links

--- src/components/post.js
+++ src/components/post.js
@@ -1,98 +1,98 @@
 export const generalVolunteer = [
   {
-    Img: "/images/avatar.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/avatar.jpg",
     Name: "Alexandra Gonzales",
     Post: "General Volunteer"
   },
 
   {
-    Img: "/images/avatar.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/avatar.jpg",
     Name: "Carol Rambaldi",
     Post: "General Volunteer"
   },
   {
-    Img: "/images/avatar.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/avatar.jpg",
     Name: "Mia Leandra",
     Post: "General Volunteer"
   }
 ];
 export const administrationVolunteer = [
   {
-    Img: "/images/AlexanderMaher.png",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/AlexanderMaher.png",
     Name: "Alexander Maher",
     Post: "Administrative Assistant"
   },
 
   {
-    Img: "/images/AlexieNilo.PNG",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/AlexieNilo.PNG",
     Name: "Alexie Nilo",
     Post: "Administrative Assistant"
   },
   {
-    Img: "/images/avatar.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/avatar.jpg",
     Name: "Atra Shahryari",
     Post: "Administrative Assistant"
   },
   {
-    Img: "/images/CindyRoberts.jpeg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/CindyRoberts.jpeg",
     Name: "Cindy Roberts",
     Post: "Social Justice Subcommittee Chair"
   },
   {
-    Img: "/images/DonnaPhan.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/DonnaPhan.jpg",
     Name: "Donna Phan",
     Post: "Administrative Assistant"
   },
   {
-    Img: "/images/FrankieSmith.png",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/FrankieSmith.png",
     Name: "Frankie Smith",
     Post: "Senior Administrative Assistant"
   }
 ];
 export const fundraisinggrantVolunteer = [
   {
-    Img: "/images/AlexandraGray.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/AlexandraGray.jpg",
     Name: "Alexandra Gray",
     Post: "Project Grant Writer & Researcher"
   },
 
   {
-    Img: "/images/AndrewShahidi.png",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/AndrewShahidi.png",
     Name: "Andrew Shahidi",
     Post: "Project Coordinator"
   },
   {
-    Img: "/images/ArnoldKim.JPG",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/ArnoldKim.JPG",
     Name: "Arnold Kim",
     Post: "Project Coordinator"
   },
   {
-    Img: "/images/BeverlyPopoola.JPG",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/BeverlyPopoola.JPG",
     Name: "Beverly popoola",
     Post: "Assistant Project Grant Writer & Researcher"
   },
   {
-    Img: "/images/avatar.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/avatar.jpg",
     Name: "Kailey Roberts",
     Post: "Grant writer"
   },
   {
-    Img: "/images/avatar.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/avatar.jpg",
     Name: "Kim Alvin De Lara",
     Post: "Project Grant Writer & Researcher"
   },
   {
-    Img: "/images/PeterViverito.jpeg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/PeterViverito.jpeg",
     Name: "Peter Viverto",
     Post: "Project Grant Writer & Researcher"
   },
   {
-    Img: "/images/avatar.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/avatar.jpg",
     Name: "Priya Ray",
     Post: "Project Grant Writer & Researcher"
   },
   {
-    Img: "/images/avatar.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/avatar.jpg",
     Name: "Zamaan Bagban",
     Post: "  Assistant Project Coordinator"
   }
@@ -100,144 +100,144 @@ export const fundraisinggrantVolunteer = [
 
 export const projectsVolunteer = [
   {
-    Img: "/images/AmandaStanley.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/AmandaStanley.jpg",
     Name: "Amanda Stanley",
     Post: "Volunteer Coordinator"
   },
 
   {
-    Img: "/images/GaytriVasal.jpeg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/GaytriVasal.jpeg",
     Name: "Gayatri Vasal",
     Post: "Program Coordinator"
   },
   {
-    Img: "/images/avatar.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/avatar.jpg",
     Name: "Hoa Anh Hoang",
     Post: "Organization Coordinator"
   },
   {
-    Img: "/images/ShruthiRamesh.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/ShruthiRamesh.jpg",
     Name: "Shruti Ramesh",
     Post: "Organization Coordinator"
   },
   {
-    Img: "/images/SmritiChadha.png",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/SmritiChadha.png",
     Name: "Smriti Chadha",
     Post: "Assistant organization coordinator "
   }
 ];
 export const communicationmarketing = [
   {
-    Img: "/images/ValenttinaCardozo.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/ValenttinaCardozo.jpg",
     Name: "Andrea Valenttina Cardozo Useche",
     Post: "Blogger/Writer"
   },
 
   {
-    Img: "/images/profile-photo_Angela Yuan.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/profile-photo_Angela%20Yuan.jpg",
     Name: "Angela Lair",
     Post: "Graphic Designer"
   },
   {
-    Img: "/images/FouadAmuni.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/FouadAmuni.jpg",
     Name: "Fouad Amuni",
     Post: "Blogger / Writer"
   },
   {
-    Img: "/images/GregHitchcock.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/GregHitchcock.jpg",
     Name: "Greg Hitchcock",
     Post: "Blogger / Writer"
   },
   {
-    Img: "/images/HannahMichaelson.png",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/HannahMichaelson.png",
     Name: "Hannah Michaelson",
     Post: "Blogger / Writer"
   },
   {
-    Img: "/images/JoannaSmykowski.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/JoannaSmykowski.jpg",
     Name: "Joanna Symkowski",
     Post: "Blogger / Writer"
   },
   {
-    Img: "/images/avatar.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/avatar.jpg",
     Name: "Kelly McNally",
     Post: "Social Media Content Creator"
   },
   {
-    Img: "/images/avatar.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/avatar.jpg",
     Name: "Kynan Jenkins",
     Post: "Assistant Marketing & Communications Coordinator"
   },
   {
-    Img: "/images/LindsayLee.png",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/LindsayLee.png",
     Name: "Lindsay Lee",
     Post: "  Social Media Content Creator"
   },
   {
-    Img: "/images/MalakEissa.jpeg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/MalakEissa.jpeg",
     Name: "Malak Eissa",
     Post: "  Social Media Content Creator"
   },
   {
-    Img: "/images/MaryhelenDiehl.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/MaryhelenDiehl.jpg",
     Name: "Maryhelen Diehl",
     Post: "Graphic Designer"
   },
   {
-    Img: "/images/avatar.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/avatar.jpg",
     Name: "MO'nique Montes",
     Post: "  Social Media Content Creator"
   },
   {
-    Img: "/images/NickLozier.jpeg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/NickLozier.jpeg",
     Name: "Nick Lozier",
     Post: "   Blogger / Writer"
   },
   {
-    Img: "/images/SamuelEffiong.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/SamuelEffiong.jpg",
     Name: "Samuel Effiong",
     Post: "   Blogger / Writer"
   },
   {
-    Img: "/images/SuryaIyer.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/SuryaIyer.jpg",
     Name: "Surya Iyer",
     Post: "   Blogger / Writer"
   },
   {
-    Img: "/images/avatar.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/avatar.jpg",
     Name: "Temeika Beasley Spruiells",
     Post: "   Blogger / Writer"
   }
 ];
 export const informationtechnology = [
   {
-    Img: "/images/AnthonyCavallaro.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/AnthonyCavallaro.jpg",
     Name: "Anthony Cavallaro",
     Post: "Azure Architect"
   },
 
   {
-    Img: "/images/AustinFriedant.JPG",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/AustinFriedant.JPG",
     Name: "Austin Friedant",
     Post: "Project Engineer"
   },
   {
-    Img: "/images/JiaShen.JPG",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/JiaShen.JPG",
     Name: "Jia(Jerry) Shen",
     Post: "Project Engineer Technician"
   },
   {
-    Img: "/images/KyleBouchet.jpeg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/KyleBouchet.jpeg",
     Name: "Kyle Bouchet",
     Post: "Project Engineer Technician"
   },
   {
-    Img: "/images/avatar.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/avatar.jpg",
     Name: "Srinath KoilaKonda",
     Post: "Azure Architect "
   },
   {
-    Img: "/images/TimoStander.jpg",
+    Img: "https://sjeiwebsitemedia.blob.core.windows.net/photos/Our%20Team/TimoStander.jpg",
     Name: "Timo Stander",
     Post: "IT Adminstrator "
   }
